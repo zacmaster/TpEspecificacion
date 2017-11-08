cc3b1089840eb58121b5746e06ace43d2f0c708e
Zacarías Samaniego <zacariassamaniego@gmail.com>
2017-11-07 21:34:20 -0300

    Se agrega la clase Usuario en negocio

--- src/main/java/negocio/Usuario.java
+++ src/main/java/negocio/Usuario.java
@@ -1,5 +1,57 @@
 package negocio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String nick;
+	private String password;
+	private String mail;
+	
+	
+	
+	@Id
+	@GeneratedValue
+    public Long getId() {
+		return id;
+    }
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
 
 }

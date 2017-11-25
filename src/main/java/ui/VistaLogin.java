b250d74fed12ae5e24ac6a0f4a3dc7f1ead9b244
Zacarías Samaniego <zacariassamaniego@gmail.com>
2017-11-25 03:12:13 -0300

    Se corrige problema con los textfields

--- src/main/java/ui/VistaLogin.java
+++ src/main/java/ui/VistaLogin.java
@@ -7,91 +7,88 @@ import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.PopupView;
+//import com.vaadin.ui.PopupView;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+//import com.vaadin.ui.VerticalLayout;
 
 import servicio.UsuarioService;
 
-import com.vaadin.ui.Button.ClickEvent;
 
 public class VistaLogin extends GridLayout implements View{
+	private UsuarioService usuarioService = new UsuarioService();
+	
 	private Button botonRegistro = new Button("Registrarse");
 	private Button botonIngreso = new Button("Ingresar");
-	private UsuarioService usuarioService = new UsuarioService();
-	private TextField userName = new TextField("Usuario");
-	private PasswordField password = new PasswordField("Password");
-	private PopupView popup = new PopupView("Pop it up", new VerticalLayout());
+	private TextField userName = new TextField();
+	private PasswordField password = new PasswordField();
+//	private PopupView popup = new PopupView("Pop it up", new VerticalLayout());
 	
 	private static final long serialVersionUID = 1L;
-	public static final String NAME = "prueba";
+	public static final String NAME = "Login";
 
 	public VistaLogin() {
 		super(3,3);
-		cargarListeners();
 		cargarComponentes();
+		cargarListeners();
 	}
 	
 	private void  cargarComponentes() {
-		Panel loginPanel = new Panel("Bienvenido a DataMining!");
+		Panel loginPanel = new Panel("Bienvenido a DataMiningo!");
 		CustomLayout content = new CustomLayout("myLayout");
-		loginPanel.setContent(content);
 		loginPanel.setWidth("80%");
 		loginPanel.setHeight("400px");
 		loginPanel.setContent(content);
 
 		
 		content.addComponent(new Label("Inicia sesión o registrate para ingresar! "), "mensajeBienvenida");
-		content.addComponent(userName);
-		content.addComponent(password);
+		content.addComponent(userName,"username");
+		content.addComponent(new TextField(),"nuevo");
+		content.addComponent(password,"password");
 		content.addComponent(botonRegistro,"registerButton");
 		content.addComponent(botonIngreso, "okbutton");
 		
 		
 		setSizeFull();
 		addComponent(loginPanel,1,1);
-		addComponent(popup);
+//		addComponent(popup);
 		addStyleName("fondoImagen");
 	}
 	
 	
 	private void cargarListeners() {
-		botonIngreso.addClickListener(new Button.ClickListener() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public void buttonClick(ClickEvent event) {
-				if(usuarioService.seAceptaLogin(userName.getValue(), password.getValue())) {
-					getUI().getNavigator().navigateTo(VistaPrincipal.NAME);
-				}					
-				else {
-					popup.setPopupVisible(true);
-//					VerticalLayout popupContent = new VerticalLayout();
-//					popupContent.addComponent(new TextField("te logeaste para elc ulo"));
-//					popupContent.addComponent(new Button("cerrar"));
-//
-//					// The component itself
-//					PopupView popup = new PopupView("Pop it up", popupContent);
-//					addComponent(popup);
-				}
-					
-			}
-		});
-
-		botonRegistro.addClickListener(new Button.ClickListener() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public void buttonClick(ClickEvent event) {
-				getUI().getNavigator().navigateTo(VistaRegistro.NAME);
-			}
-		});
-
+		botonIngreso.addClickListener(clickEvent -> clickbotonIngreso());
+		botonRegistro.addClickListener(clickEvent -> clickbotonRegistro());
 	}
 	
-	private void cargarPopUp() {
-		// Content for the PopupView
-
-		// The component itself
-		
-		addComponent(popup);
+	private void clickbotonRegistro() {
+		getUI().getNavigator().navigateTo(VistaRegistro.NAME);
 	}
+
+	private void clickbotonIngreso() {
+		if(usuarioService.seAceptaLogin(userName.getValue(), password.getValue())) {
+			getUI().getNavigator().navigateTo(VistaPrincipal.NAME);
+		}					
+		else {
+//			popup.setPopupVisible(true);
+//			VerticalLayout popupContent = new VerticalLayout();
+//			popupContent.addComponent(new TextField("te logeaste para elc ulo"));
+//			popupContent.addComponent(new Button("cerrar"));
+//
+//			// The component itself
+//			PopupView popup = new PopupView("Pop it up", popupContent);
+//			addComponent(popup);
+		}
+	}
+	
+	
+	
+//	private void cargarPopUp() {
+//		// Content for the PopupView
+//
+//		// The component itself
+//		
+//		addComponent(popup);
+//	}
+	
+	
 }
